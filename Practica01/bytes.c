bdbf9ab1275b4fb1b83b3451d02a73f3dd37b817
ricardorodab <ricardorodab19@gmail.com>
2017-02-13 01:03:07 -0600

    Practica01 - Test agregados y ejercicio 01 listo. Falta modulo de polinomios

--- Practica01/bytes.c
+++ Practica01/bytes.c
@@ -1,5 +1,5 @@
 /* -------------------------------------------------------------------
- * second.c
+ * bytes.c
  * version 1.0
  * Copyright (C) 2017  Jose Ricardo Rodriguez Abreu.
  * Facultad de Ciencias,
@@ -27,30 +27,58 @@
  */
 
 /**
- * @file second.c
+ * @file bytes.c
  * @author Jose Ricardo Rodriguez Abreu
- * @date 6 Feb 2017
+ * @date 13 Feb 2017
  * @brief File containing the second exercise from the first practice for the
- * "Concurrent computing" class.
+ * "Cryptography and security" class.
  *
- * Este programa utiliza la biblioteca Pthread para crear 50 hilos y modificar
- * una variable que compartan todas en memoria. Tiene como objetivo primario
- * identificar los posibles riesgos de la programacion concurrente.
+ * Este programa recibe dos archivos para generar dos archivos mas que
+ * sirven para practicar los metodos xor y producto de bytes.
  *
  * El programa usa el estandar de documentacion que define el uso de 
  * doxygen.
  *
- * @see https://github.com/ricardorodab/concurrente
+ * @see https://github.com/ricardorodab/Cripto
  */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
+/**
+ * @brief Constante char.
+ *
+ * Usamos esta palabra para rellenar los espacios vacios del archivo
+ * mas chico.
+ */
 #define PALABRA "beetlejuicebe"
+
+/**
+ * @brief Constante int.
+ *
+ * Este es el tamanio de la palabra que usaremos para rellenar el archivo
+ * mas chico. Esta variable sirve por si deseamos cambiar la palabra.
+ */
 #define PALABRA_TAMANIO 11
 
+/**
+ * @brief Constante int-byte.
+ *
+ * Esta constante se usa para realizar la operacion de producto. 
+ * Este es el binario equivalente a 10101010
+ */
+#define BYTE_CONSTANTE 0xAA
 
+/**
+ * @brief Empareja archivos de entrada.
+ *
+ * Funcion que escribe en el archivo mas chico si es necesario
+ * para que tengan las mismas dimensiones.
+ *
+ * @param argv - Es la entrada de los archivos que deseamos comparar.
+ * @return 0 si es que no tuvo ningun problema, 1 en otro caso.
+ */
 int empareja_archivos(char *argv[])
 {
 int linea;
@@ -75,18 +103,7 @@ int linea;
   while(int_file1 != -1 || int_file2 != -1){    
     char *letra = PALABRA;
     if(int_file1 == -1){
-      /*if(linea != -1){
-	rewind(file1);
-	int tmp = 0;
-	while(tmp < linea){
-	int_file1 = fscanf(file1,"%c",char_file1);
-	fprintf(file1,"%c",char_file1[0]);
-	tmp++;
-	}
-	//Falta tener que guardar en un archivo tmp
-	//el texto y luego copiarlo.
-	linea = -1;
-	}*/
+      //rewind(file1); //Esto es para regresar la marca de lectura.
       int_file2 = fscanf(file2,"%c",char_file2);
       if(int_file2 != -1){
 	if(char_file2[0] == '\n'){
@@ -117,7 +134,15 @@ int linea;
   return 0;
 }
 
-
+/**
+ * @brief XOR entre dos archivos.
+ *
+ * Funcion que escribe un archivo llamado xor.out que contiene
+ * bytes resultantes de la funcion XOR dado dos archivos de entrada.
+ *
+ * @param archivos - Es la entrada de los archivos que deseamos hacer XOR.
+ * @return 0 si es que no tuvo ningun problema, 1 en otro caso.
+ */
 int realiza_xor(char *archivos[])
 {
   FILE *file1;
@@ -153,7 +178,67 @@ int realiza_xor(char *archivos[])
   return 0;
 }
 
+/**
+ * @brief Realiza multiplicacion por un byte constante.
+ *
+ * Funcion que escribe un archivo llamado multiplicacion.out que contiene
+ * bytes resultantes de la funcion multiplicacion definida como el producto
+ * de dos polinomios en base 2 dado un byte.
+ *
+ * @param entrada - Es la entrada de el archivo a realizar el producto.
+ * @return 0 si es que no tuvo ningun problema, 1 en otro caso.
+ */
+int realiza_multiplicacion(char *entrada)
+{
+  FILE *file;
+  FILE *multi;
+  char *nombre_archivo = "multiplicacion.out";
+  unsigned char char_file[1];
+  unsigned char *salida_multi;
+  int int_file;
+  int int_resultado = 0;
+  int recorrimiento;
+  unsigned int cast_entrada;
+  
+  multi = fopen(nombre_archivo,"wb+");
+  file = fopen(entrada,"rb");
+  if(file == NULL || multi == NULL){
+    printf("Algun archivo no existe o existe un problema al abrirlo.\n");
+    return 1;
+  }
+  do{
+    int_file = fscanf(file,"%c",char_file);
+    if(int_file == -1)
+      break;
+    int i;
+    recorrimiento = 1;
+    cast_entrada = (char_file[0] ^ 0);
+    int_resultado = 0;
+    for(i = 0; i < 8; i++){
+      if(BYTE_CONSTANTE & recorrimiento){
+	int_resultado ^= cast_entrada;
+      }
+      cast_entrada = cast_entrada << 1;
+      recorrimiento = recorrimiento << 1;      
+    }
+    // 65280 en binario es 1111111100000000
+    int COTA_SUPERIOR = 65280;
+    //Hacer el modulo aqui al int_resultado.
+    salida_multi = (unsigned char*)&int_resultado;
+    fprintf(multi,"%c",*salida_multi);
+  }while(int_file != -1);
+  return 0;
+}
 
+/**
+ * @brief Funcion principal.
+ *
+ * Es la funcion principal del programa y que define su comportamiento.
+ *
+ * @param argc - Es el numero de argumentos que recibe el programa.
+ * @param argv - Son los argumentos desde la linea de comandos que recibe.
+ * @return 0 si es que no tuvo ningun problema, 1 en otro caso.
+ */
 int main(int argc, char *argv[])
 {
   //Si el parametro no es valido terminamos.
@@ -169,6 +254,6 @@ int main(int argc, char *argv[])
   }
 
   realiza_xor(argv);
-  
+  realiza_multiplicacion(argv[1]);
   return 0;
-}
+} //Fin de bytes.c
